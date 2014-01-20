7671c427cab56a14a6624c74e38f85fb26b8ff4b
Olaf Dietsche <olaf--mail.github@olafdietsche.de>
2014-01-20 13:59:25 +0100

    Add libraries menu

--- webfiddle.js
+++ webfiddle.js
@@ -3,13 +3,37 @@ var css = new editor_panel('css', 'css-switch', 'ace/mode/css');
 var js = new editor_panel('js', 'js-switch', 'ace/mode/javascript');
 js.script = document.getElementById('js-script');
 
+var libraries = document.getElementById('libraries');
 var web = document.getElementById('web');
 var update = document.getElementById('update');
 update.addEventListener('click', update_frame);
 update_frame();
 
+function include_libraries() {
+    var inputs = libraries.getElementsByTagName('input');
+    var js = '', css = '';
+    for (var i = 0; i < inputs.length; ++i) {
+        var type = inputs[i].type;
+        if ((type == 'checkbox') && inputs[i].checked) {
+            var url = inputs[i].getAttribute('data-url');
+            var ext = url.lastIndexOf('.');
+            switch (url.substr(ext)) {
+            case '.js':
+                js += '<script type="text/javascript" src="' + url + '"></script>\n';
+                break;
+            case '.css':
+                css += '<link rel="stylesheet" href="' + url + '">\n';
+                break;
+            }
+        }
+    }
+
+    return js + css;
+}
+
 function build_page_source() {
     var source = '<!DOCTYPE html>\n<html>\n<head>\n';
+    source += include_libraries();
     var style = css.getValue();
     if (style)
         source += '<style>' + style + '</style>\n';
